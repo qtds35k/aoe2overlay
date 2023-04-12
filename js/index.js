aeab4d4258f897aa8c59fca038ab185453226665
qtds35k <sh.wu@nyu.edu>
2023-04-12 00:19:33 -0700

    Increase query matches to 1000 games for more accurate player stats .

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 async function main() {
     const urlParams = new URLSearchParams(window.location.search);
     const corsProxyUrl = 'https://api.allorigins.win/raw?url=';
-    // const corsProxyUrl = 'https://cors-proxy.htmldriven.com/?url=';  // cors-proxy service is disabled.
     // const gitRepoName = ''; // for local testing
     const gitRepoName = '/aoe2overlay'; // for github page endpoint
     const stringsLookupPath = `${gitRepoName}/resource/strings.json`;
@@ -16,7 +15,7 @@ async function main() {
 
         document.getElementById("playerName1").innerText = playerStats.playerName;
         setTextShadow("playerName1", playerStats.lastPlayerColor);
-        // setting opponent color here; if overlay's not displaying ongoing match, opponent might have had more games than streamer
+        // Setting opponent color here; if overlay's not displaying ongoing match, opponent might have had more games than streamer
         setTextShadow("playerName2", playerStats.lastOpponentColor);
         document.getElementById("playerCurrentElo1").innerText = playerStats.playerCurrentElo;
         document.getElementById("playerMaxElo1").innerText = playerStats.playerMaxElo;
@@ -81,7 +80,7 @@ async function main() {
     // Get player's name, current/max elo, total games played, winrate, and used civs in last 5 ranked 1v1 games.
     async function getPlayerStats(profileId) {
         const urlPlayerStatus = `https://aoe2.net/api/nightbot/rank?game=aoe2de&leaderboard_id=3&profile_id=${profileId}&flag=false`;
-        const urlRatingHistory = `https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&profile_id=${profileId}&count=100`;
+        const urlRatingHistory = `https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&profile_id=${profileId}&count=1000`;
         const urlMatches = `https://aoe2.net/api/player/matches?game=aoe2de&count=50&profile_id=${profileId}`;
 
         const regexPlayerName = /.*(?=\s\(\d+\))/;
